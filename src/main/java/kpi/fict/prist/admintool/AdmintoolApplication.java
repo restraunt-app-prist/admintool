044a332fe417dc1c693404bd1440585d9e223097
Bogdan Zaranik <bogdan.zaranik@appliedmethods.biz>
2024-12-20 09:46:50 +0200

    initial

---
+++ src/main/java/kpi/fict/prist/admintool/AdmintoolApplication.java
@@ -0,0 +1,13 @@
+package kpi.fict.prist.admintool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmintoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmintoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kpi/fict/prist/admintool/AdmintoolApplicationTests.java
@@ -0,0 +1,13 @@
+package kpi.fict.prist.admintool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmintoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
