b0816f04ea48d7d0ac5f621d686ca3614f8a052c
Bogdan Zaranik <bogdan.zaranik@appliedmethods.biz>
2024-12-29 14:54:41 +0200

    cors

--- src/main/java/kpi/fict/prist/admintool/config/SecurityConfig.java
+++ src/main/java/kpi/fict/prist/admintool/config/SecurityConfig.java
@@ -57,8 +57,9 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5174"));
-        configuration.setAllowedMethods(List.of("GET","POST"));
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
